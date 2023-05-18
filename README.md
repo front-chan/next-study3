c85fc00073d21a46e835d4be9446d94a718667d5
front-chan <chan.jor.zz@gmail.com>
2023-05-18 14:15:21 +0900

    css modules study 1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+# NextJS Introduction
+
+강의: 노마더코더
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,20 +1,25 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
     const router = useRouter();
-    console.log(router);
+    // console.log(router);
 
     return (
-        <nav>
+        <nav className={styles.nav}>
             <Link
-                style={{ color: router.pathname === "/" ? "red" : "blue" }}
+                className={`${styles.link} 
+                  ${router.pathname === "/" ? styles.active : null}`}
                 href="/"
             >
                 Home
             </Link>
             <Link
-                style={{ color: router.pathname === "/about" ? "red" : "blue" }}
+                className={[
+                    styles.link,
+                    router.pathname === "/about" ? styles.active : null,
+                ].join(" ")}
                 href="/about"
             >
                 About
