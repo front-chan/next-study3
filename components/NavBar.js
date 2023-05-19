e32001dac8f1586a3a275ba43c63fb79cfe159fb
front-chan <chan.jor.zz@gmail.com>
2023-05-19 20:49:15 +0900

    fetching data study

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,29 +3,50 @@ import { useRouter } from "next/router";
 
 export default function NavBar() {
     const router = useRouter();
-    console.log(router.pathname);
+    // console.log(router.pathname);
 
     return (
         <nav>
-            <Link
-                className={router.pathname === "/" ? "active" : null}
-                href="/"
-            >
-                Home
-            </Link>
-            <Link
-                className={router.pathname === "/about" ? "active" : null}
-                href="/about"
-            >
-                About
-            </Link>
-            <style jsx>{`
-                a {
-                    text-decoration: none;
+            <img src="/vercel.svg" />
+            <div>
+                <Link
+                    href="/"
+                    className={router.pathname === "/" ? "active" : ""}
+                >
+                    Home
+                </Link>
+                <Link
+                    href="/about"
+                    className={router.pathname === "/about" ? "active" : ""}
+                >
+                    About
+                </Link>
+            </div>
+            <style jsx global>{`
+                nav {
+                    display: flex;
+                    gap: 10px;
+                    flex-direction: column;
+                    align-items: center;
+                    padding-top: 20px;
+                    padding-bottom: 10px;
+                    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+                        rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+                }
+                img {
+                    max-width: 100px;
+                    margin-bottom: 5px;
+                }
+                nav a {
+                    font-weight: 600;
+                    font-size: 18px;
                 }
                 .active {
                     color: tomato;
-                    text-decoration: none;
+                }
+                nav div {
+                    display: flex;
+                    gap: 10px;
                 }
             `}</style>
         </nav>
