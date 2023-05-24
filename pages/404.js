93051dd3cad5a418523bdaa8b1ea423e664ceaf3
front-chan <chan.jor.zz@gmail.com>
2023-05-24 14:54:30 +0900

    404 page study

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Seo from "../components/Seo";
+
+export default function NotFound() {
+    return (
+        <div>
+            <Seo title="Not Found" />
+            <h4>what are u doing here?</h4>
+        </div>
+    );
+}
